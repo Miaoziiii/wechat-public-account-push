075688b24676a776badd45cfc5e5963245da1dcb
Miaoziiii <149610077+Miaoziiii@users.noreply.github.com>
2023-11-02 10:09:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,11 +7,12 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wxb8dfbba1eecffff7',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'fb3f43a017c55b06dc84d06f3e6e9f7d',
+  APP_SECRET: '',
 
   PROVINCE: '山东',
   CITY: '泰安',
@@ -21,9 +22,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '管熙汶',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-Oo16IE6JvNOKr7x4ARzoeG9JWs',
+      id: '8f34928ab15d485e9a1693b2dd522962',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'NNCLzVdR4VKEynyAB1poMYUdQh49y22Zq2cyhqKp4Qc',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-16',
       festivals: [
